98cdaf8262b2bcc125062a3b6c70f0ec86db6b30
Erik Borgeteien Hansen <github@borgeteienhansen.no>
2020-09-29 15:08:57 +0200

    Initial commit with calibration

---
+++ Bot.py
@@ -0,0 +1,37 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, ColorSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+import time
+
+class BroBot():
+    def __init__(self):
+        self.brobot = DriveBase(left_motor=Motor(Port.B), right_motor=Motor(Port.C), wheel_diameter=55, axle_track=127)
+        self.left_color_sensor, self.right_color_sensor = ColorSensor(Port.S4), ColorSensor(Port.S2)
+        self.ev3 = EV3Brick()
+        self.RED, self.GREEN, self.BLUE = 50, 50, 50
+
+    def wait_for_button(self):
+        while True:
+            if self.ev3.buttons.pressed():
+                break
+            continue
+
+    def calibrate(self):
+        self.ev3.screen.print("CALIBRATE RGB-SENSORS:\nL-SENSOR: WHITE\nR-SENSOR: BLACK\nPUSH A BUTTON\nTO CONTINUE")
+        self.wait_for_button()
+
+        RED_ON_WHITE, GREEN_ON_WHITE, BLUE_ON_WHITE = self.left_color_sensor.rgb()
+        RED_ON_BLACK, GREEN_ON_BLACK, BLUE_ON_BLACK = self.right_color_sensor.rgb()
+
+        self.RED = (RED_ON_WHITE + RED_ON_BLACK) // 2
+        self.GREEN = (GREEN_ON_WHITE + GREEN_ON_BLACK) // 2
+        self.BLUE = (BLUE_ON_WHITE + BLUE_ON_BLACK) // 2
+
+    def is_black(self, color_sensor):
+        (red, green, blue) = color_sensor.rgb()
+        if red < self.RED and green < self.GREEN and blue < self.BLUE:
+            return True
+        else:
+            return False
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env pybricks-micropython
+from Bot import Bot
+
+robot = Bot()
+
+robot.calibrate()
